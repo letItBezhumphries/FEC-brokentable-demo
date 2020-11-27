e38407e963efadaa35c6f4611aacc412c5ffebb0
letItBezhumphries <ezhumphries@gmail.com>
2020-11-27 23:52:54 +0000

    added resources dev infrastructure to build first ami that will be used for serving restaurant data

--- dev/variables.tf
+++ dev/variables.tf
@@ -2,27 +2,12 @@ variable "AWS_REGION" {
   default = "us-west-2"
 }
 
-#variable "PATH_TO_PUBLIC_KEY" {
-#  default = "devkey"
-#}
-
-#variable "PATH_TO_PRIVATE_KEY" {
-#  default = "devkey.pub"
-#}
-
 variable "ENV" {
   description = "The environment setting for this project"
   type = string
   default = "dev"
 }
 
-#variable "DB_INSTANCE_CLASS" {
-#  default = "db.t2.micro"
-#}
+variable "MYSQL_PASSWORD" {}
 
-#variable "INSTANCE_TYPE" {
-#  default = "t2.micro"
-#}
-
-
-variable "RDS_PASSWORD" {}
+variable "MYSQL_USERNAME" {}

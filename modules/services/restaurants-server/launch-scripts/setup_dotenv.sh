5bbb32632a52b82482a8a07f8c499668b10c9a9f
Eric Humphries <32630651+letItBezhumphries@users.noreply.github.com>
2020-12-02 15:44:35 -0800

    Update setup_dotenv.sh

--- modules/services/restaurants-server/launch-scripts/setup_dotenv.sh
+++ modules/services/restaurants-server/launch-scripts/setup_dotenv.sh
@@ -8,7 +8,7 @@ DOTENV_USERNAME="RDS_USERNAME="${RDS_USERNAME}
 DOTENV_PORT="RDS_PORT="${RDS_PORT}
 DOTENV_DB_NAME="DB_NAME="${DB_NAME}
 BASHENV_RDS_PASSWORD="export RDS_PASSWORD="${RDS_PASSWORD}
-BASHENV_RDS_HOST="export RDS_HOSTNAME="${RDS_HOST}
+BASHENV_RDS_HOST="export RDS_HOST="${RDS_HOST}
 
 # Create .env file for DOTENV variables and append password and hostname into .env
 sudo -u ubuntu touch /home/ubuntu/FEC-Restaurant-Info-Module/.env
@@ -23,7 +23,7 @@ echo $DOTENV_PORT >> /home/ubuntu/FEC-Restaurant-Info-Module/.env
 echo $DOTENV_DB_NAME >> /home/ubuntu/FEC-Restaurant-Info-Module/.env
 
 # Run the shell script inside FEC-Reviews-Module that will create the mysql database
-#sudo mysql -u ${RDS_USERNAME} -h ${RDS_HOST} -p${RDS_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}" 
+sudo mysql -u ${RDS_USERNAME} -h ${RDS_HOST} -p${RDS_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}" 
 
 # Run the seed script to seed Review_Module with records.
 #cd /home/ubuntu/FEC-Reviews-Module && npm run seed

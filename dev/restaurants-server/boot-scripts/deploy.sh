554b44f12eb8df55f494d0a04fb5ca06355eea5c
Eric Humphries <32630651+letItBezhumphries@users.noreply.github.com>
2020-12-12 18:47:52 -0800

    Update deploy.sh

--- dev/restaurants-server/boot-scripts/deploy.sh
+++ dev/restaurants-server/boot-scripts/deploy.sh
@@ -14,7 +14,3 @@ sudo apt-get install -y nodejs
 # clone down restaurant-info-module
 sudo -u ubuntu git clone https://github.com/letItBezhumphries/FEC-Restaurant-Info-Module.git
 
-# create a .my.cnf file in home directory
-sudo -u ubuntu touch /home/ubuntu/.my.cnf
-
-echo "[client]" >> /home/ubuntu/.my.cnf
